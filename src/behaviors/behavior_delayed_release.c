677ced2b93d345767cd5efe993552b5ec8552d73
Ryan Purpura <ryan@rpurp.com>
2026-02-14 21:28:27 -0800

    wip

--- src/behaviors/behavior_delayed_release.c
+++ src/behaviors/behavior_delayed_release.c
@@ -24,7 +24,7 @@ struct behavior_delayed_release_config {
 
 struct behavior_delayed_release_data {
     bool trigger_key_pressed;
-    bool prequisite_key_pressed;
+    bool prerequisite_key_pressed;
     bool delayed_release_behavior_active;
 };
 
@@ -41,11 +41,11 @@ static int on_delayed_release_binding_pressed(struct zmk_behavior_binding *bindi
     if (binding->param1 == DELAYED_RELEASE_PREREQUISITE_KEY) {
         // Prerequisite key pressed; trigger its behavior and record it in the state.
         zmk_behavior_invoke_binding(&cfg->prerequisite_behavior, event, true);
-        state->prequisite_key_pressed = true;
+        state->prerequisite_key_pressed = true;
     } else if (binding->param1 == DELAYED_RELEASE_TRIGGER_KEY) {
-        // If the delayed release behavior has not yet been triggered,
+        // If the delayed release behavior has not yet been triggered, and the prerequisite key is pressed,
         // trigger it now and record it in the state.
-        if (!state->delayed_release_behavior_active) {
+        if (!state->delayed_release_behavior_active && state->prerequisite_key_pressed) {
             zmk_behavior_invoke_binding(&cfg->delayed_release_behavior, event, true);
             state->delayed_release_behavior_active = true;
         }
@@ -74,13 +74,13 @@ static int on_delayed_release_binding_released(struct zmk_behavior_binding *bind
         }
 
         zmk_behavior_invoke_binding(&cfg->prerequisite_behavior, event, false);
-        state->prequisite_key_pressed = false;
+        state->prerequisite_key_pressed = false;
     } else if (binding->param1 == DELAYED_RELEASE_TRIGGER_KEY) {
         // Trigger key released.
         zmk_behavior_invoke_binding(&cfg->trigger_behavior, event, false);
         state->trigger_key_pressed = false;
 
-        if (!state->prequisite_key_pressed && state->delayed_release_behavior_active) {
+        if (!state->prerequisite_key_pressed && state->delayed_release_behavior_active) {
             // (From above) in this case, the prerequisite key was released, but the trigger
             // key is still pressed. Now we can release the delayed release behavior.
             zmk_behavior_invoke_binding(&cfg->delayed_release_behavior, event, false);
@@ -96,19 +96,34 @@ static const struct behavior_driver_api behavior_delayed_release_driver_api = {
     .binding_released = on_delayed_release_binding_released,
 };
 
+#define TRANSFORM_ENTRY(idx, node)                                                             \
+{                                                                                              \
+    .behavior_dev = DEVICE_DT_NAME(DT_INST_PHANDLE_BY_IDX(node, bindings, idx)),               \
+    .param1 = COND_CODE_0(DT_INST_PHA_HAS_CELL_AT_IDX(node, bindings, idx, param1), (0),       \
+    (DT_INST_PHA_BY_IDX(node, bindings, idx, param1))),                                        \
+    .param2 = COND_CODE_0(DT_INST_PHA_HAS_CELL_AT_IDX(node, bindings, idx, param2), (0),       \
+    (DT_INST_PHA_BY_IDX(node, bindings, idx, param2))),                                        \
+}
+
 #define BEHAVIOR_DELAYED_RELEASE_INST(n)                                                    \
     static struct behavior_delayed_release_config behavior_delayed_release_config_##n = {   \
-        .prerequisite_behavior = DT_PROP(DT_INST_PHANDLE_BY_IDX(n, bindings, 0), label),    \
-        .delayed_release_behavior = DT_PROP(DT_INST_PHANDLE_BY_IDX(n, bindings, 1), label), \
-        .trigger_behavior = DT_PROP(DT_INST_PHANDLE_BY_IDX(n, bindings, 2), label),         \
-        .index = n;                                                                         \
+        .prerequisite_behavior = TRANSFORM_ENTRY(0, n),                                     \
+        .delayed_release_behavior = TRANSFORM_ENTRY(1, n),                                  \
+        .trigger_behavior = TRANSFORM_ENTRY(2, n),                                          \
+        .index = n,                                                                         \
+    };                                                                                      \
+    static struct behavior_delayed_release_data behavior_delayed_release_data_##n = {       \
+        .trigger_key_pressed = false,                                                       \
+        .prerequisite_key_pressed = false,                                                  \
+        .delayed_release_behavior_active = false,                                           \
     };                                                                                      \
     BEHAVIOR_DT_INST_DEFINE(n,                                                              \
                             behavior_delayed_release_init,                                  \
                             NULL,                                                           \
                             &behavior_delayed_release_data_##n,                             \
                             &behavior_delayed_release_config_##n,                           \
-                            POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT                \
+                            POST_KERNEL,                                                    \
+                            CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,                            \
                             &behavior_delayed_release_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(BEHAVIOR_DELAYED_RELEASE_INST)
